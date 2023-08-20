64baaf6d8e1467a36dd775257da5460f324a213a
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-08-21 05:13:51 +0800

    mahasiswa with component tag's

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -24,7 +24,7 @@ class MahasiswaController extends Controller
      */
     public function index()
     {
-        return view('mahasiswa.index', [
+        return view('mahasiswa.mahasiswa', [
             'mahasiswas' => Mahasiswa::all()
         ]);
     }
--- resources/views/mahasiswa/edit.blade.php
+++ resources/views/mahasiswa/edit.blade.php
@@ -1,66 +1,23 @@
-@extends('layouts.layout-vertical.main')
-@section('title', 'Ubah Data Mahasiswa')
-@section('content')
-    @component('components.page-heading')
-        @slot('title', 'Ubah Data Mahasiswa')
-        @slot('subtitle', 'Halaman ubah data mahasiswa')
-        <li class="breadcrumb-item ">
-            <a href="{{ route('mahasiswa.index') }}">Mahasiswa</a>
-        </li>
-        <li class="breadcrumb-item active" aria-current="page">
-            Ubah Data Mahasiswa
-        </li>
-    @endcomponent
-
-    @component('components.form')
-        @slot('route', route('mahasiswa.store'))
-        @slot('method', 'POST')
-        @component('components.form-group')
-            @slot('col', 'col-md-6 col-12')
-            @slot('label', 'NIM')
-            @slot('invalidFeedback', $errors->first('NIM'))
-            @component('components.input')
-                @slot('type', 'text')
-                @slot('name', 'NIM')
-                @slot('value', $mahasiswa->NIM)
-                @slot('placeholder', 'Masukkan NIM')
-                @slot('required', true)
-            @endcomponent
-        @endcomponent
-        @component('components.form-group')
-            @slot('col', 'col-md-6 col-12')
-            @slot('label', 'Nama Mahasiswa')
-            @slot('invalidFeedback', $errors->first('name'))
-            @component('components.input')
-                @slot('type', 'text')
-                @slot('name', 'name')
-                @slot('value', $mahasiswa->name)
-                @slot('placeholder', 'Masukkan Nama Mahasiswa')
-                @slot('required', true)
-            @endcomponent
-        @endcomponent
-        @component('components.form-group')
-            @slot('col', 'col-12')
-            @slot('label', 'Tanggal Lahir')
-            @slot('invalidFeedback', $errors->first('tgl_lahir'))
-            @component('components.input')
-                @slot('type', 'date')
-                @slot('name', 'date_of_birth')
-                @slot('value', $mahasiswa->date_of_birth)
-                @slot('placeholder', 'Masukkan Tanggal Lahir')
-                @slot('required', true)
-            @endcomponent
-        @endcomponent
-        @component('components.form-group')
-            @slot('col', 'col-12')
-            @slot('label', 'Alamat')
-            @slot('invalidFeedback', $errors->first('alamat'))
-            @component('components.textarea')
-                @slot('name', 'address')
-                @slot('value', $mahasiswa->address)
-                @slot('placeholder', 'Masukkan Alamat')
-                @slot('required', true)
-            @endcomponent
-        @endcomponent
-    @endcomponent
-@endsection
+<x-app-layout title="Ubah Data Mahasiswa">
+    <x-page-heading title="Ubah Data Mahasiswa" subtitle="Halaman ubah data mahasiswa">
+        <li class="breadcrumb-item "><a href="{{ route('mahasiswa.index') }}">Mahasiswa</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Ubah Data Mahasiswa</li>
+    </x-page-heading>
+    <x-form route="{{ route('mahasiswa.update') }}" method="POST" overrideMethod="PUT">
+        <x-form-group col="col-md-6 col-12" label="NIM" :invalid-feedback="$errors->first('NIM')">
+            <x-input type="text" name="NIM" :value="$mahasiswa->NIM" placeholder="Masukkan NIM"
+                required="true"></x-input>
+        </x-form-group>
+        <x-form-group col="col-md-6 col-12" label="Nama Mahasiswa" :invalidFeedback="$errors->first('name')">
+            <x-input type="text" name="name" :value="$mahasiswa->name" placeholder="Masukkan Nama Mahasiswa"
+                required="true"></x-input>
+        </x-form-group>
+        <x-form-group label="Tanggal Lahir" :invalidFeedback="$errors->first('date_of_birth')">
+            <x-input type="date" name="date_of_birth" :value="$mahasiswa->date_of_birth" placeholder="Masukkan Tanggal Lahir"
+                required="true"></x-input>
+        </x-form-group>
+        <x-form-group label="Alamat" :invalidFeedback="$errors->first('address')">
+            <x-textarea name="address" :value="$mahasiswa->address" placeholder="Masukkan Alamat" required="true"></x-textarea>
+        </x-form-group>
+    </x-form>
+</x-app-layout>
